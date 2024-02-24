67820b7d629ae5fc6a53247c8d7add3e066855fb
tjani8 <122315491+tjani8@users.noreply.github.com>
2024-02-24 22:42:59 +0200

    Aidon modifications
    kvar -> kVAr
    kvarh -> kVArh
    add unit for reactive_power_delivered and returned

--- src/dsmr/fields.h
+++ src/dsmr/fields.h
@@ -238,8 +238,8 @@ namespace dsmr
       static constexpr char dm3[] = "dm3";
       static constexpr char GJ[] = "GJ";
       static constexpr char MJ[] = "MJ";
-      static constexpr char kvar[] = "kvar";
-      static constexpr char kvarh[] = "kvarh";
+      static constexpr char kVAr[] = "kVAr";
+      static constexpr char kVArh[] = "kVArh";
       static constexpr char kVA[] = "kVA";
       static constexpr char VA[] = "VA";
       static constexpr char s[] = "s";
@@ -302,25 +302,25 @@ namespace dsmr
     /*
  * Extra fields used for Luxembourg and Lithuania
  */
-    DEFINE_FIELD(total_imported_energy, FixedValue, ObisId(1, 0, 3, 8, 0), FixedField, units::kvarh, units::kvarh);
-    /* Meter Reading Reactive energy delivered to client (Tariff 1) in 0,001 kvarh */
-    DEFINE_FIELD(reactive_energy_delivered_tariff1, FixedValue, ObisId(1, 0, 3, 8, 1), FixedField, units::kvarh, units::kvarh);
-    /* Meter Reading Reactive energy delivered to client (Tariff 2) in 0,001 kvarh */
-    DEFINE_FIELD(reactive_energy_delivered_tariff2, FixedValue, ObisId(1, 0, 3, 8, 2), FixedField, units::kvarh, units::kvarh);
-    /* Meter Reading Reactive energy delivered to client (Tariff 3) in 0,001 kvarh */
-    DEFINE_FIELD(reactive_energy_delivered_tariff3, FixedValue, ObisId(1, 0, 3, 8, 3), FixedField, units::kvarh, units::kvarh);
-    /* Meter Reading Reactive energy delivered to client (Tariff 4) in 0,001 kvarh */
-    DEFINE_FIELD(reactive_energy_delivered_tariff4, FixedValue, ObisId(1, 0, 3, 8, 4), FixedField, units::kvarh, units::kvarh);
+    DEFINE_FIELD(total_imported_energy, FixedValue, ObisId(1, 0, 3, 8, 0), FixedField, units::kVArh, units::kVArh);
+    /* Meter Reading Reactive energy delivered to client (Tariff 1) in 0,001 kVArh */
+    DEFINE_FIELD(reactive_energy_delivered_tariff1, FixedValue, ObisId(1, 0, 3, 8, 1), FixedField, units::kVArh, units::kVArh);
+    /* Meter Reading Reactive energy delivered to client (Tariff 2) in 0,001 kVArh */
+    DEFINE_FIELD(reactive_energy_delivered_tariff2, FixedValue, ObisId(1, 0, 3, 8, 2), FixedField, units::kVArh, units::kVArh);
+    /* Meter Reading Reactive energy delivered to client (Tariff 3) in 0,001 kVArh */
+    DEFINE_FIELD(reactive_energy_delivered_tariff3, FixedValue, ObisId(1, 0, 3, 8, 3), FixedField, units::kVArh, units::kVArh);
+    /* Meter Reading Reactive energy delivered to client (Tariff 4) in 0,001 kVArh */
+    DEFINE_FIELD(reactive_energy_delivered_tariff4, FixedValue, ObisId(1, 0, 3, 8, 4), FixedField, units::kVArh, units::kVArh);
 
-    DEFINE_FIELD(total_exported_energy, FixedValue, ObisId(1, 0, 4, 8, 0), FixedField, units::kvarh, units::kvarh);
-    /* Meter Reading Reactive energy delivered by client (Tariff 1) in 0,001 kvarh */
-    DEFINE_FIELD(reactive_energy_returned_tariff1, FixedValue, ObisId(1, 0, 4, 8, 1), FixedField, units::kvarh, units::kvarh);
-    /* Meter Reading Reactive energy delivered by client (Tariff 2) in 0,001 kvarh */
-    DEFINE_FIELD(reactive_energy_returned_tariff2, FixedValue, ObisId(1, 0, 4, 8, 2), FixedField, units::kvarh, units::kvarh);
-    /* Meter Reading Reactive energy delivered by client (Tariff 3) in 0,001 kvarh */
-    DEFINE_FIELD(reactive_energy_returned_tariff3, FixedValue, ObisId(1, 0, 4, 8, 3), FixedField, units::kvarh, units::kvarh);
-    /* Meter Reading Reactive energy delivered by client (Tariff 4) in 0,001 kvarh */
-    DEFINE_FIELD(reactive_energy_returned_tariff4, FixedValue, ObisId(1, 0, 4, 8, 4), FixedField, units::kvarh, units::kvarh);
+    DEFINE_FIELD(total_exported_energy, FixedValue, ObisId(1, 0, 4, 8, 0), FixedField, units::kVArh, units::kVArh);
+    /* Meter Reading Reactive energy delivered by client (Tariff 1) in 0,001 kVArh */
+    DEFINE_FIELD(reactive_energy_returned_tariff1, FixedValue, ObisId(1, 0, 4, 8, 1), FixedField, units::kVArh, units::kVArh);
+    /* Meter Reading Reactive energy delivered by client (Tariff 2) in 0,001 kVArh */
+    DEFINE_FIELD(reactive_energy_returned_tariff2, FixedValue, ObisId(1, 0, 4, 8, 2), FixedField, units::kVArh, units::kVArh);
+    /* Meter Reading Reactive energy delivered by client (Tariff 3) in 0,001 kVArh */
+    DEFINE_FIELD(reactive_energy_returned_tariff3, FixedValue, ObisId(1, 0, 4, 8, 3), FixedField, units::kVArh, units::kVArh);
+    /* Meter Reading Reactive energy delivered by client (Tariff 4) in 0,001 kVArh */
+    DEFINE_FIELD(reactive_energy_returned_tariff4, FixedValue, ObisId(1, 0, 4, 8, 4), FixedField, units::kVArh, units::kVArh);
 
     /* Tariff indicator electricity. The tariff indicator can also be used
  * to switch tariff dependent loads e.g boilers. This is the
@@ -335,8 +335,8 @@ namespace dsmr
     /*
  * Extra fields used for Luxembourg and Lithuania
  */
-    DEFINE_FIELD(reactive_power_delivered, FixedValue, ObisId(1, 0, 3, 7, 0), FixedField, units::kvar, units::kvar);
-    DEFINE_FIELD(reactive_power_returned, FixedValue, ObisId(1, 0, 4, 7, 0), FixedField, units::kvar, units::kvar);
+    DEFINE_FIELD(reactive_power_delivered, FixedValue, ObisId(1, 0, 3, 7, 0), FixedField, units::kVAr, units::kVAr);
+    DEFINE_FIELD(reactive_power_returned, FixedValue, ObisId(1, 0, 4, 7, 0), FixedField, units::kVAr, units::kVAr);
 
     /* The actual threshold Electricity in kW. Removed in 4.0.7 / 4.2.2 / 5.0 */
     DEFINE_FIELD(electricity_threshold, FixedValue, ObisId(0, 0, 17, 0, 0), FixedField, units::kW, units::W);
@@ -446,24 +446,24 @@ namespace dsmr
     /*
  * LUX and Lithuania
  */
-    /* TODO: by IEC 62056 unit's shoudl be kvar, safe to change? */
+    /* TODO: by IEC 62056 unit's shoudl be kVAr, safe to change? */
     /* Instantaneous reactive power L1 (+Q) in W resolution */
-    DEFINE_FIELD(reactive_power_delivered_l1, FixedValue, ObisId(1, 0, 23, 7, 0), FixedField, units::none, units::none);
+    DEFINE_FIELD(reactive_power_delivered_l1, FixedValue, ObisId(1, 0, 23, 7, 0), FixedField, units::kVAr, units::kVAr);
     /* Instantaneous reactive power L2 (+Q) in W resolution */
-    DEFINE_FIELD(reactive_power_delivered_l2, FixedValue, ObisId(1, 0, 43, 7, 0), FixedField, units::none, units::none);
+    DEFINE_FIELD(reactive_power_delivered_l2, FixedValue, ObisId(1, 0, 43, 7, 0), FixedField, units::kVAr, units::kVAr);
     /* Instantaneous reactive power L3 (+Q) in W resolution */
-    DEFINE_FIELD(reactive_power_delivered_l3, FixedValue, ObisId(1, 0, 63, 7, 0), FixedField, units::none, units::none);
+    DEFINE_FIELD(reactive_power_delivered_l3, FixedValue, ObisId(1, 0, 63, 7, 0), FixedField, units::kVAr, units::kVAr);
 
     /*
  * LUX and Lithuania
  */
-    /* TODO: by IEC 62056 unit's shoudl be kvar, safe to change? */
+    /* TODO: by IEC 62056 unit's shoudl be kVAr, safe to change? */
     /* Instantaneous reactive power L1 (-Q) in W resolution */
-    DEFINE_FIELD(reactive_power_returned_l1, FixedValue, ObisId(1, 0, 24, 7, 0), FixedField, units::none, units::none);
+    DEFINE_FIELD(reactive_power_returned_l1, FixedValue, ObisId(1, 0, 24, 7, 0), FixedField, units::kVAr, units::kVAr);
     /* Instantaneous reactive power L2 (-Q) in W resolution */
-    DEFINE_FIELD(reactive_power_returned_l2, FixedValue, ObisId(1, 0, 44, 7, 0), FixedField, units::none, units::none);
+    DEFINE_FIELD(reactive_power_returned_l2, FixedValue, ObisId(1, 0, 44, 7, 0), FixedField, units::kVAr, units::kVAr);
     /* Instantaneous reactive power L3 (-Q) in W resolution */
-    DEFINE_FIELD(reactive_power_returned_l3, FixedValue, ObisId(1, 0, 64, 7, 0), FixedField, units::none, units::none);
+    DEFINE_FIELD(reactive_power_returned_l3, FixedValue, ObisId(1, 0, 64, 7, 0), FixedField, units::kVAr, units::kVAr);
 
     /* Apparent instantaneous power (+S) in kVA resolution */
     DEFINE_FIELD(apparent_delivery_power, FixedValue, ObisId(1, 0, 9, 7, 0), FixedField, units::kVA, units::VA);
@@ -558,14 +558,14 @@ namespace dsmr
     /*Current quart-hourly energy consumption*/
     DEFINE_FIELD(active_energy_import_current_average_demand, FixedValue, ObisId(1, 0, 1, 4, 0), FixedField, units::kW, units::W);
     DEFINE_FIELD(active_energy_export_current_average_demand, FixedValue, ObisId(1, 0, 2, 4, 0), FixedField, units::kW, units::W);
-    DEFINE_FIELD(reactive_energy_import_current_average_demand, FixedValue, ObisId(1, 0, 3, 4, 0), FixedField, units::kvar, units::kvar);
-    DEFINE_FIELD(reactive_energy_export_current_average_demand, FixedValue, ObisId(1, 0, 4, 4, 0), FixedField, units::kvar, units::kvar);
+    DEFINE_FIELD(reactive_energy_import_current_average_demand, FixedValue, ObisId(1, 0, 3, 4, 0), FixedField, units::kVAr, units::kVAr);
+    DEFINE_FIELD(reactive_energy_export_current_average_demand, FixedValue, ObisId(1, 0, 4, 4, 0), FixedField, units::kVAr, units::kVAr);
     DEFINE_FIELD(apparent_energy_import_current_average_demand, FixedValue, ObisId(1, 0, 9, 4, 0), FixedField, units::kVA, units::VA);
     DEFINE_FIELD(apparent_energy_export_current_average_demand, FixedValue, ObisId(1, 0, 10, 4, 0), FixedField, units::kVA, units::VA);
     DEFINE_FIELD(active_energy_import_last_completed_demand, FixedValue, ObisId(1, 0, 1, 5, 0), FixedField, units::kW, units::W);
     DEFINE_FIELD(active_energy_export_last_completed_demand, FixedValue, ObisId(1, 0, 2, 5, 0), FixedField, units::kW, units::W);
-    DEFINE_FIELD(reactive_energy_import_last_completed_demand, FixedValue, ObisId(1, 0, 3, 5, 0), FixedField, units::kvar, units::kvar);
-    DEFINE_FIELD(reactive_energy_export_last_completed_demand, FixedValue, ObisId(1, 0, 4, 5, 0), FixedField, units::kvar, units::kvar);
+    DEFINE_FIELD(reactive_energy_import_last_completed_demand, FixedValue, ObisId(1, 0, 3, 5, 0), FixedField, units::kVAr, units::kVAr);
+    DEFINE_FIELD(reactive_energy_export_last_completed_demand, FixedValue, ObisId(1, 0, 4, 5, 0), FixedField, units::kVAr, units::kVAr);
     DEFINE_FIELD(apparent_energy_import_last_completed_demand, FixedValue, ObisId(1, 0, 9, 5, 0), FixedField, units::kVA, units::VA);
     DEFINE_FIELD(apparent_energy_export_last_completed_demand, FixedValue, ObisId(1, 0, 10, 5, 0), FixedField, units::kVA, units::VA);
 
